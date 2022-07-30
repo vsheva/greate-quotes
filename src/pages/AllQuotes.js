62e519f8f3246f024849d80e38978a0b023188e5
vsheva <valera.sheva@gmail.com>
2022-07-31 02:06:13 +0300

    Send and get quote data via http

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,16 +1,46 @@
+import {useEffect} from 'react';
 import QuoteList from '../components/quotes/QuoteList'
 import React from "react";
+import useHttp from "../hooks/use-http" //custom hook
+import {getAllQuotes} from "../lib/api"
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound"; // from api
 
-const DUMMY_QUOTES =[
-    {id:"a1", author:"Valerii", text:"Learning React is great!"},
-    {id:"a2", author:"Shevchenko", text:"Learning React is fun!"},
-];
 
-const AllQuotes =()=> {
-    return(
-    <QuoteList quotes={DUMMY_QUOTES}/>
-    )
+//
+const AllQuotes = () => {
+    const {sendRequest, status, data: loadedQuotes, error} = useHttp(
+        getAllQuotes, true) //*
+
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
+
+    if (status === "pending") {
+        return (
+            <div className="centered">
+                <LoadingSpinner/>
+            </div>
+        );
+    }
+
+    if(error) {return <p className="centered focused">{error}</p>}
+
+    if(status==='completed' &&(!loadedQuotes || loadedQuotes.length===0)) {
+      return <NoQuotesFound />
+    }
+
+
+    //return <QuoteList quotes={DUMMY_QUOTES}/>;
+    return <QuoteList quotes={loadedQuotes}/>;
 }
 
 export default AllQuotes;
 
+/*
+const DUMMY_QUOTES = [
+    {id: "a1", author: "Valerii", text: "Learning React is great!"},
+    {id: "a2", author: "Shevchenko", text: "Learning React is fun!"},
+];
+*/
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -2,31 +2,48 @@ import {useParams, Route, Link, useRouteMatch} from "react-router-dom";
 import React from "react";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-
-const DUMMY_QUOTES =[
-    {id:"a1", author:"Valerii", text:"Learning React is great!"},
-    {id:"a2", author:"Shevchenko", text:"Learning React is fun!"},
-];
-
-
+import useHttp from "../hooks/use-http";
+import {getSingleQuote} from "../lib/api";
+import {useEffect} from "react";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
     const match =useRouteMatch(); //{path: '/quotes/:quoteId', url: '/quotes/a2', isExact: true, params: {…}
-    const params = useParams();
-    console.log("match", match)
+    const params = useParams(); //{quoteId: '-N8GFVIBGaJsIzcXRm2F'}
+
+    const {quoteId} =params;
+
+    const {sendRequest, status, data:loadedQuote, error}=useHttp(
+        getSingleQuote,
+        true
+    );
+
+    useEffect(()=>{
+       sendRequest(quoteId);
+   },[sendRequest,quoteId])
+
+    if (status==="pending") {
+        return <div className="centered">
+            <LoadingSpinner />
+        </div>
+    }
+
+    if(error) {return <p className="centered ">{error}</p>}
+
+    if (!loadedQuote) {return <p>No quote found!</p> }
+
+   // const quote = DUMMY_QUOTES.find(elem=>elem.id === params.quoteId); //a1 из DUMMY_QUOTES === введенному нами маршруту a1  //потому что в App.js из пути url
+
+   // console.log("quote", quote) //{id: 'a1', author: 'Valerii', text: 'Learning React is great!'}
 
 
-    const quote = DUMMY_QUOTES.find(elem=>elem.id === params.quoteId); //a1 из DUMMY_QUOTES === введенному нами маршруту a1  //потому что в App.js из пути url
-
-    console.log("quote", quote) //{id: 'a1', author: 'Valerii', text: 'Learning React is great!'}
-    if(!quote) {
+    if(!loadedQuote.text) {
         return <p>No quote found!</p>
     }
 
     return (
         <section>
-            <HighlightedQuote text={quote.text} author={quote.author} />
+            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
 
             <Route path={`${match.path}`} exact>
 
@@ -54,3 +71,10 @@ export default QuoteDetail;
  */
 
 // <Route path="./quotes/:quoteId/comments"> <p> Наши комментарии</p> </Route>
+
+/*
+const DUMMY_QUOTES =[
+    {id:"a1", author:"Valerii", text:"Learning React is great!"},
+    {id:"a2", author:"Shevchenko", text:"Learning React is fun!"},
+];
+*/
